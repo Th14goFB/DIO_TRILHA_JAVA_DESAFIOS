58c48a2a719a4828fab8239a3d5d4f715c410cc8
Thigas <106130463+Th14goFB@users.noreply.github.com>
2023-10-10 17:09:25 -0300

    Contador.java

--- Contador.java
+++ Contador.java
@@ -2,7 +2,7 @@ package thigas.DESAFIOS_JAVA;
 import java.util.Scanner;
 
 public class Contador {
-    public static void main (String[] args){
+    public static void main (String[] args) throws Exception{
 
     Scanner terminal = new Scanner(System.in);
 
@@ -11,20 +11,25 @@ public class Contador {
 
     System.out.println("Digite outro algarimo:");
     int numeroDois = terminal.nextInt();
-        contagem(numeroUm, numeroDois);
 
+      try{    
+        contagem(numeroUm, numeroDois);
+      }catch(ParametrosInvalidosException e){
+          System.err.println("O segundo parâmetro não pode ser maior que o primeiro.");
+      }
 }
     static void contagem (int numeroUm, int numeroDois)throws ParametrosInvalidosException  {
-    try{
+        if(numeroUm > numeroDois) {
+            throws new ParametrosInvalidosException()
+        }
+        
         int contagem = numeroDois - numeroUm;
         System.out.println(contagem);
 
         for(int i=1; i<=contagem;i++) {
         System.out.println("Imprimindo o algarismo " + i);
         }
-    }catch(ParametrosInvalidosException e){
-        System.out.println("Operação invalida.");
-    }
+
         System.out.println("Fim da operação.");
 
     } 
